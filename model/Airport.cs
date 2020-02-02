c975d01d9424897792233414ef96a8e4ff506f64
Cesar Canales <sleepata6@gmail.com>
2020-02-02 14:30:14 -0500

    Added infraestructure to show flight data in table

--- model/Airport.cs
+++ model/Airport.cs
@@ -10,5 +10,23 @@ namespace model
     class Airport
     {
         private String iataID;
+        private List<Flight> flights;
+
+        public Airport(String iataID)
+        {
+            this.iataID = iataID;
+            flights = new List<Flight>();
+        }
+        public List<Flight> getFlights()
+        {
+            return flights;
+        }
+
+        public void addFlight(string airLineID, string origin, string destination, string departureTime,
+            string arriveTime, string date, string distance, string flightTime, bool isLate)
+        {
+            Flight flight = new Flight(airLineID, origin, destination, departureTime, arriveTime, date, distance, flightTime, isLate);
+            flights.Add(flight);
+        }
     }
 }
--- model/DataManager.cs
+++ model/DataManager.cs
@@ -17,13 +17,53 @@ namespace model
         public DataManager()
         {
             airports = new Dictionary<string, Airport>();
-            
-        
+            airports["CLT"] = new Airport("CLT");
+            airports["FLL"] = new Airport("FLL");
+            airports["RSW"] = new Airport("RSW");
+            airports["PHX"] = new Airport("PHX");
+            airports["ATL"] = new Airport("ATL");
+            airports["DTW"] = new Airport("DTW");
+            airports["PBI"] = new Airport("PBI");
+            airports["PIT"] = new Airport("PIT");
+            airports["PHL"] = new Airport("PHL");
+            airports["TPA"] = new Airport("TPA");
+            airports["MIA"] = new Airport("MIA");
+            airports["LAX"] = new Airport("LAX");
+            airports["DAL"] = new Airport("DAL");
+            airports["RNO"] = new Airport("RNO");
+            airports["LAS"] = new Airport("LAS");
+            airports["DEN"] = new Airport("DEN");
+            airports["SLC"] = new Airport("SLC");
+            airports["MSP"] = new Airport("MSP");
+            airports["IAD"] = new Airport("IAD");
+            airports["MCO"] = new Airport("MCO");
+            airports["CLE"] = new Airport("CLE");
+            airports["BOI"] = new Airport("BOI");
+            airports["ORD"] = new Airport("ORD");
+            airports["TUS"] = new Airport("TUS");
+            airports["FAI"] = new Airport("FAI");
+            airports["ICT"] = new Airport("ICT");
+            airports["AVL"] = new Airport("AVL");
+            airports["CVG"] = new Airport("CVG");
+            airports["KTN"] = new Airport("KTN");
+            airports["LAN"] = new Airport("LAN");
+            airports["MEM"] = new Airport("MEM");
+            airports["LBB"] = new Airport("LBB");
+            airports["RDD"] = new Airport("RDD");
+            airports["ROC"] = new Airport("ROC");
+            airports["SIT"] = new Airport("SIT");
+            airports["SAN"] = new Airport("SAN");
+            airports["YUM"] = new Airport("YUM");
+            airports["BNA"] = new Airport("BNA");
+            airports["TWF"] = new Airport("TWF");
+            airports["ABE"] = new Airport("ABE");
+            airports["MSO"] = new Airport("MSO");
+            airports["SAF"] = new Airport("SAF");
         }
 
-        public List<Flight> getFlights(String iata)
+        public List<Flight> getAirportFlights(String iata)
         {
-            return airports[iata]
+            return airports[iata].getFlights();
         }
 
         public void readInfo()
@@ -40,7 +80,7 @@ namespace model
                     string[] args = line.Split(',');
 
                     string airLineID = args[7];
-                    string origin = args[15];
+                    string origin = args[14];
                     string destination = args[24];
                     string departureTime = args[30];
                     string arriveTime = args[41];
@@ -48,8 +88,10 @@ namespace model
                     string distance = args[54];
                     string flightTime = args[52];
                     Boolean isLate = false;
-                    Flight flight = new Flight(airLineID, origin, destination, departureTime, arriveTime, date, distance, flightTime, isLate);
-                    flights.Add(flight);
+                    if (airports.ContainsKey(origin) && airports.ContainsKey(destination))
+                    {
+                        airports[origin].addFlight(airLineID, origin, destination, departureTime, arriveTime, date, distance, flightTime, isLate);
+                    }
                     count++;
                 }
 
