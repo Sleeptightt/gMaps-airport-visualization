bdf603f3bb7910e26712e686393302dcc24a5237
Cesar Canales <sleepata6@gmail.com>
2020-02-02 17:42:56 -0500

    Implemented searches

---
+++ userInterface/airportScreen.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace userInterface
+{
+    partial class airportScreen
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gMap1 = new GMap.NET.WindowsForms.GMapControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.arrivingOrDepartingFlights = new System.Windows.Forms.ComboBox();
+            this.filterTextBox = new System.Windows.Forms.TextBox();
+            this.filterComboBox = new System.Windows.Forms.ComboBox();
+            this.filterButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gMap1
+            // 
+            this.gMap1.Bearing = 0F;
+            this.gMap1.CanDragMap = true;
+            this.gMap1.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMap1.GrayScaleMode = false;
+            this.gMap1.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMap1.LevelsKeepInMemory = 5;
+            this.gMap1.Location = new System.Drawing.Point(13, 13);
+            this.gMap1.MarkersEnabled = true;
+            this.gMap1.MaxZoom = 12;
+            this.gMap1.MinZoom = 2;
+            this.gMap1.MouseWheelZoomEnabled = true;
+            this.gMap1.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMap1.Name = "gMap1";
+            this.gMap1.NegativeMode = false;
+            this.gMap1.PolygonsEnabled = true;
+            this.gMap1.RetryLoadTile = 0;
+            this.gMap1.RoutesEnabled = true;
+            this.gMap1.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMap1.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMap1.ShowTileGridLines = false;
+            this.gMap1.Size = new System.Drawing.Size(968, 616);
+            this.gMap1.TabIndex = 0;
+            this.gMap1.Zoom = 0D;
+            this.gMap1.Load += new System.EventHandler(this.gMap1_Load);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1018, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(133, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "AIRPORT INFORMATION";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataView
+            // 
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(1021, 45);
+            this.dataView.Name = "dataView";
+            this.dataView.Size = new System.Drawing.Size(386, 376);
+            this.dataView.TabIndex = 2;
+            this.dataView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataView_CellContentClick);
+            // 
+            // arrivingOrDepartingFlights
+            // 
+            this.arrivingOrDepartingFlights.FormattingEnabled = true;
+            this.arrivingOrDepartingFlights.Items.AddRange(new object[] {
+            "Arriving flights",
+            "Departing flights"});
+            this.arrivingOrDepartingFlights.Location = new System.Drawing.Point(1275, 10);
+            this.arrivingOrDepartingFlights.Name = "arrivingOrDepartingFlights";
+            this.arrivingOrDepartingFlights.Size = new System.Drawing.Size(158, 21);
+            this.arrivingOrDepartingFlights.TabIndex = 3;
+            this.arrivingOrDepartingFlights.Text = "Select a display option";
+            this.arrivingOrDepartingFlights.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // filterTextBox
+            // 
+            this.filterTextBox.Location = new System.Drawing.Point(1021, 502);
+            this.filterTextBox.Name = "filterTextBox";
+            this.filterTextBox.Size = new System.Drawing.Size(175, 20);
+            this.filterTextBox.TabIndex = 4;
+            this.filterTextBox.Text = "Enter what you\'re searching for";
+            this.filterTextBox.Visible = false;
+            // 
+            // filterComboBox
+            // 
+            this.filterComboBox.FormattingEnabled = true;
+            this.filterComboBox.Items.AddRange(new object[] {
+            "None",
+            "Airline ID",
+            "Date",
+            "Delay"});
+            this.filterComboBox.Location = new System.Drawing.Point(1233, 502);
+            this.filterComboBox.Name = "filterComboBox";
+            this.filterComboBox.Size = new System.Drawing.Size(150, 21);
+            this.filterComboBox.TabIndex = 5;
+            this.filterComboBox.Text = "Select the search criteria";
+            this.filterComboBox.Visible = false;
+            // 
+            // filterButton
+            // 
+            this.filterButton.Location = new System.Drawing.Point(1177, 541);
+            this.filterButton.Name = "filterButton";
+            this.filterButton.Size = new System.Drawing.Size(75, 23);
+            this.filterButton.TabIndex = 6;
+            this.filterButton.Text = "Filter";
+            this.filterButton.UseVisualStyleBackColor = true;
+            this.filterButton.Visible = false;
+            this.filterButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // airportScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1445, 641);
+            this.Controls.Add(this.filterButton);
+            this.Controls.Add(this.filterComboBox);
+            this.Controls.Add(this.filterTextBox);
+            this.Controls.Add(this.arrivingOrDepartingFlights);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gMap1);
+            this.Name = "airportScreen";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private GMap.NET.WindowsForms.GMapControl gMap1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataView;
+        private System.Windows.Forms.ComboBox arrivingOrDepartingFlights;
+        private System.Windows.Forms.TextBox filterTextBox;
+        private System.Windows.Forms.ComboBox filterComboBox;
+        private System.Windows.Forms.Button filterButton;
+    }
+}
+
